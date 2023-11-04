10e567a3d4aa2a3a0123341d80145cc769af3513
angenue <angelica1nuesi@gmail.com>
2023-11-03 23:53:11 -0400

    removed irrelevant code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,30 +1,18 @@
 // src/server.ts
-import dotenv from 'dotenv';
 import app from "./app";
 import validateEnv from './util/validateEnv';
 import mongoose from "mongoose";
-dotenv.config(); 
+
 const env = validateEnv();
+
 const port = env.PORT;
 
-let connectionString;
+const mongoConnectionString: string = env.MONGO_CONNECTION_STRING as string;
 
-if (process.env.NODE_ENV === 'test') {
-  connectionString = process.env.MONGO_CONNECTION_STRING_TEST;
-} else {
-  connectionString = process.env.MONGO_CONNECTION_STRING;
-}
-
-if (!connectionString) {
-  throw new Error('Database connection string is not set.');
-}
-  mongoose.connect(connectionString)
-  .then(() => {
-
-    console.log("Mongoose connected");
+mongoose.connect(mongoConnectionString).then(() => {
+  console.log("Mongoose connected");
   app.listen(port, () => {
     console.log("Server running on port: " + port);
   });
-  
-  })
-  .catch(err => console.error(err));
+})
+.catch(console.error);
