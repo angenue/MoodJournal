8779eddceff41707e7237ca0b5c734aaa5e7b8c9
angenue <angelica1nuesi@gmail.com>
2023-11-06 22:02:46 -0500

    testing popup closing when deleting journal

--- frontend/src/components/calendar/MonthlyCalendar.test.tsx
+++ frontend/src/components/calendar/MonthlyCalendar.test.tsx
@@ -134,4 +134,52 @@ describe('MonthlyCalendar', () => {
       expect(screen.queryByText('Tue Oct 03 2023')).not.toBeInTheDocument();
     });
   });
+
+
+  it('closes popup when the journal is deleted', async () => {
+    fetchJournals.mockResolvedValue([
+        { _id: "6545b9c4afbb87deccba9063",
+        mood: "happy",
+        journalEntry: "Hello this is a test",
+        date: selectedDate, },
+      ]);
+
+    render(
+      <MonthlyCalendar
+        year={2023}
+        month={9} // October (0-indexed)
+        moodData={mockMoodData}
+        updateMoodData={mockUpdateMoodData}
+      />
+    );
+
+    const dayTiles = screen.getAllByRole('button');
+    const dayTile = dayTiles.find(tile => tile.textContent === '3');
+
+    if (!dayTile) {
+        throw new Error('Day tile for October 3rd not found');
+      }
+
+    fireEvent.click(dayTile);
+
+  await waitFor(() => {
+    expect(screen.getByText('Tue Oct 03 2023')).toBeInTheDocument();
+  });
+
+  const deleteButton = screen.getByRole('button', { name: /delete journal/i });
+  fireEvent.click(deleteButton);
+
+
+    await waitFor(() => {
+        expect(screen.getByText('Confirm Delete')).toBeInTheDocument();
+      });
+
+     // Wait for the confirmation modal to appear and confirm deletion
+  const confirmDeleteButton = await screen.findByRole('button', { name: /yes, delete/i });
+  fireEvent.click(confirmDeleteButton);
+
+    await waitFor(() => {
+      expect(screen.queryByText('Tue Oct 03 2023')).not.toBeInTheDocument();
+    });
+  });
 });
