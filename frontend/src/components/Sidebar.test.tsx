b938cd3bd5ba291efa1509c828a8cd5da2eb2b74
angenue <angelica1nuesi@gmail.com>
2023-11-07 23:35:36 -0500

    testing for sidebar links

--- frontend/src/components/Sidebar.test.tsx
+++ frontend/src/components/Sidebar.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import Sidebar from './Sidebar';
@@ -20,38 +20,76 @@ jest.mock("../utils/journal_api");
 // Mock the toast messages
 jest.mock("../utils/toastMessage");
 
+// ... other imports and mocks
+
 describe('Sidebar Component', () => {
-  const onLogoutSuccessfulMock = jest.fn();
+    const onLogoutSuccessfulMock = jest.fn();
+  
+    beforeEach(() => {
+      // Reset the mocks before each test
+      mockedNavigate.mockReset();
+      onLogoutSuccessfulMock.mockReset();
+     logout.mockResolvedValue({}); // Ensure this resolves
+    });
+  
+    it('navigates to "/" when the logout button is clicked', async () => {
+      render(
+        <MemoryRouter>
+          <Sidebar onLogoutSuccessful={onLogoutSuccessfulMock} />
+        </MemoryRouter>
+      );
+  
+      // Find and click the logout button
+      const logoutButton = screen.getByText('Logout');
+      userEvent.click(logoutButton);
+  
+      // Wait for any asynchronous actions to complete
+      await waitFor(() => {
+        // Assert that the onLogoutSuccessful callback was called
+        expect(onLogoutSuccessfulMock).toHaveBeenCalled();
+      });
+  
+      // Assert that navigate was called with the correct argument
+      expect(mockedNavigate).toHaveBeenCalledWith('/');
+    });
 
-  beforeEach(() => {
-    mockedNavigate.mockReset();
-    onLogoutSuccessfulMock.mockReset();
-    logout.mockResolvedValue({});
+    it('navigates to "/Calendar" when the Calendar button is clicked', async () => {
+        render(
+            <MemoryRouter>
+              <Sidebar onLogoutSuccessful={() => {}} />
+            </MemoryRouter>
+          );
+    
+        const calendarButton = screen.getByText('Calendar');
+        userEvent.click(calendarButton);
+    
+        // eslint-disable-next-line testing-library/no-node-access
+        const homeLink = screen.getByText('Calendar').closest('a');
+        expect(homeLink).toHaveAttribute('href', '/Calendar');
+      });
+      it('has the correct link to the Home page', () => {
+        render(
+          <MemoryRouter>
+            <Sidebar onLogoutSuccessful={() => {}} />
+          </MemoryRouter>
+        );
+    
+        // eslint-disable-next-line testing-library/no-node-access
+        const homeLink = screen.getByText('Home').closest('a');
+        expect(homeLink).toHaveAttribute('href', '/Home');
+      });
+
+      it('has the correct link to the Graph page', () => {
+        render(
+          <MemoryRouter>
+            <Sidebar onLogoutSuccessful={() => {}} />
+          </MemoryRouter>
+        );
+    
+        // eslint-disable-next-line testing-library/no-node-access
+        const homeLink = screen.getByText('Graph').closest('a');
+        expect(homeLink).toHaveAttribute('href', '/Graph');
+      });
+
   });
-
-  it('navigates to "/" when the logout button is clicked', async () => {
-    render(
-      <MemoryRouter>
-        <Sidebar onLogoutSuccessful={onLogoutSuccessfulMock} />
-      </MemoryRouter>
-    );
-
-    // Find and click the logout button
-    const logoutButton = screen.getByText('Logout');
-    userEvent.click(logoutButton);
-
-    // Assert that the logout function was called
-    expect(JournalsApi.logout).toHaveBeenCalled();
-
-    // Assert that the onLogoutSuccessful callback was called
-    expect(onLogoutSuccessfulMock).toHaveBeenCalled();
-
-    // Assert that navigate was called with the correct argument
-    expect(mockedNavigate).toHaveBeenCalledWith('/');
-
-    // Optionally, check if the errorMessage function was called if the logout failed
-    // expect(errorMessage).toHaveBeenCalledWith("Logout Unsuccessful");
-  });
-
-  // ... Other tests for sidebar links ...
-});
+  
